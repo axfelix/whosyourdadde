986df507179088fae2330ba656d9bbbba58f2ae5
Alex Garnett <axfelix@gmail.com>
2021-01-07 20:39:14 -0800

    enable passing values to URL

--- whosyourdadde.py
+++ whosyourdadde.py
@@ -1,5 +1,6 @@
 from flask import *
 from flask import render_template
+from flask import Markup
 from whosyourdad import whosyourdad
 from waitress import serve
 
@@ -16,7 +17,12 @@ def getdad():
         else:
             return jsonify('')
 
-    return render_template('index.html')
+    return render_template('index.html', prefill="james bennet nyt")
+
+@app.route('/<string:text>', methods= ["GET"])
+def getdad_url(text):
+    dad = whosyourdad(text)
+    return render_template('index.html', prefill=text, value=Markup(dad))
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=5000)
